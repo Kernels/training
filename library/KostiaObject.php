c62669c36ea8046d154bddca57af8e4707cd8184
Anisimow Kostya <kostiaGt@mail.ru>
2012-06-01 14:18:10 +0300

    Добавление класса KostyaObjectAbstract Добавление метода showHello

--- library/KostiaObject.php
+++ library/KostiaObject.php
@@ -1,13 +1,27 @@
 <?php
 
-class KostiaObject {
-     public function __construct()
-     {
-         print "Hello";
-         $this->returnHelloWorld();
-     }
-     //егор
-     public function helloWorld(){
-         echo 'Hello World';
-     }
+if (!define('KOSTYAOBJECTABSTRACT')) {
+    include_once 'KostyaObjectAbstract.php';
+}
+
+class KostiaObject extends KostyaObjectAbstract
+{
+
+    public function __construct()
+    {
+        print "Hello";
+        $this->returnHelloWorld();
+    }
+
+    public function showHello()
+    {
+        echo 'hello';
+    }
+
+    //егор
+    public function helloWorld()
+    {
+        echo 'Hello World';
+    }
+
 }
---
+++ library/KostyaObjectAbstract.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of KostyaObjectAbstract
+ *
+ * @author kot
+ */
+abstract class KostyaObjectAbstract
+{
+    abstract public function showHello();
+}
+
+?>
